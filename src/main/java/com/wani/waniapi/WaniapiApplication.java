10c8e03bfad9a02b874df2462bc200a4a9d31539
hophoet <hophoet@gmail.com>
2021-03-14 17:30:38 +0000

    intial commit

---
+++ src/main/java/com/wani/waniapi/WaniapiApplication.java
@@ -0,0 +1,13 @@
+package com.wani.waniapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaniapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaniapiApplication.class, args);
+	}
+
+}
