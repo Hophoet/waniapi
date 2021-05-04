c519de1d6f684717d3dfeb42ed1ffcbe331a8fff
hophoet <hophoet@gmail.com>
2021-05-04 17:50:59 +0000

    (controller) - add deleteUser endpoint to AdminController with ADMIN ROLE authorization to delete the user

--- src/main/java/com/wani/waniapi/api/controllers/AdminController.java
+++ src/main/java/com/wani/waniapi/api/controllers/AdminController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +28,20 @@ public class AdminController {
     public List<User> getUsers(){
         List<User> users = userRepository.findAll();
         return users;
+    }
+
+    @DeleteMapping("/users/{id}/delete")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity deleteUser(@PathVariable String id){
+         try {
+            if(!userRepository.findById(id).isPresent()){
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
+            }
+            userRepository.deleteById(id);
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
     }
 }
