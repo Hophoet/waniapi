e81dede1451112c00dcfa0ddbcb55fdb24f7688a
hophoet <hophoet@gmail.com>
2021-05-05 08:58:30 +0000

    (controller) - add createUser endpoint to AdminController with ADMIN permission required to create new user

--- src/main/java/com/wani/waniapi/api/controllers/AdminController.java
+++ src/main/java/com/wani/waniapi/api/controllers/AdminController.java
@@ -1,7 +1,10 @@
 package com.wani.waniapi.api.controllers;
 
 import com.wani.waniapi.auth.models.User;
+import com.wani.waniapi.auth.models.Role;
+import com.wani.waniapi.auth.models.ERole;
 import com.wani.waniapi.auth.repository.UserRepository;
+import com.wani.waniapi.auth.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,17 +16,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.wani.waniapi.auth.playload.response.MessageResponse;
+import com.wani.waniapi.auth.playload.request.SignupRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.List;
+import java.util.Set;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class AdminController {
+
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+    
+    @Autowired
+    PasswordEncoder encoder;
 
     @GetMapping("/users")
     @PreAuthorize("hasRole('ADMIN')")
@@ -102,5 +117,66 @@ public class AdminController {
         userValues.setEmail(email);
         //TODO update the user
         return ResponseEntity.ok(userRepository.save(userValues));
+    }
+
+
+    @PostMapping("/users/create")
+    public ResponseEntity<?> createUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+        //check the user signup reference
+        if(signUpRequest.getReference() != null){
+            //set the user reference
+            user.setReference(signUpRequest.getReference());
+        }
+
+        Set<String> strRoles = signUpRequest.getRoles();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User created successfully!"));
     }
 }
