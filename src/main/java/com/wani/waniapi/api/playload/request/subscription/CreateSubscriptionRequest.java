2cae597b8fd57ac33c4b765db3ba9248db973204
hophoet <hophoet@gmail.com>
2021-05-22 11:42:51 +0000

    (requests) - add CreateSubscriptionRequest as the subcription request data validator

---
+++ src/main/java/com/wani/waniapi/api/playload/request/subscription/CreateSubscriptionRequest.java
@@ -0,0 +1,49 @@
+package com.wani.waniapi.api.playload.request.subscription;
+
+import javax.validation.constraints.NotBlank;
+
+public class CreateSubscriptionRequest {
+    @NotBlank
+    private String userId;
+
+    @NotBlank
+    private String subscriptionPlanId;
+
+    @NotBlank
+    private String paymentMethodId;
+
+    private  Integer createdAt;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSubscriptionPlanId() {
+        return subscriptionPlanId;
+    }
+
+    public void setSubscriptionPlanId(String subscriptionPlanId) {
+        this.subscriptionPlanId = subscriptionPlanId;
+    }
+
+    public String getPaymentMethodId() {
+        return paymentMethodId;
+    }
+
+    public void setPaymentMethodId(String paymentMethodId) {
+        this.paymentMethodId = paymentMethodId;
+    }
+
+    public Integer getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Integer createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
