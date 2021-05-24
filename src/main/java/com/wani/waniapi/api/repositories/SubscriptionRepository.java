91e1b8ad44efae7a822678cac4cd7cd5d7f8ddcc
hophoet <hophoet@gmail.com>
2021-05-24 16:24:07 +0000

    (repositories) - add findByUserId method for subcriptions gettion by the userId<subscriptions related to a user>

--- src/main/java/com/wani/waniapi/api/repositories/SubscriptionRepository.java
+++ src/main/java/com/wani/waniapi/api/repositories/SubscriptionRepository.java
@@ -5,7 +5,10 @@ import com.wani.waniapi.api.models.Subscription;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface SubscriptionRepository extends MongoRepository<Subscription, String> {
+    List<Subscription> findByUserId(String userId);
+
 
 }
