28a9ae3915da5fa9984b7aff6fb2c6f3892a416e
hophoet <hophoet@gmail.com>
2021-06-01 01:00:54 +0000

    (controllers) - add setUserProfileImage endpoint to the user profile setting

--- src/main/java/com/wani/waniapi/auth/controllers/AuthController.java
+++ src/main/java/com/wani/waniapi/auth/controllers/AuthController.java
@@ -9,6 +9,10 @@ import com.wani.waniapi.auth.playload.request.UpdateRequest;
 import com.wani.waniapi.auth.playload.response.JwtResponse;
 import com.wani.waniapi.auth.playload.response.ErrorResponse;
 
+import com.wani.waniapi.api.models.File;
+
+import com.wani.waniapi.api.repositories.FileRepository;
+
 import com.wani.waniapi.auth.playload.response.MessageResponse;
 import com.wani.waniapi.auth.repository.RoleRepository;
 import com.wani.waniapi.auth.repository.UserRepository;
@@ -22,18 +26,26 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpStatus;
 
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.bson.types.Binary;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    FileRepository fileRepository;
+
     @Autowired
     AuthenticationManager authenticationManager;
 
@@ -86,7 +98,8 @@ public class AuthController {
                 userDetails.getFirstName(),
                 userDetails.getLastName(),
                 userDetails.getAddress(),
-                userDetails.getIsActive()
+                userDetails.getIsActive(),
+                " jdkf"
             )
         );
     }
@@ -194,7 +207,9 @@ public class AuthController {
                 userDetails.getFirstName(),
                 userDetails.getLastName(),
                 userDetails.getAddress(),
-                userDetails.getIsActive()
+                userDetails.getIsActive(),
+                " jdkf"
+
             )
         );
 
@@ -277,4 +292,64 @@ public class AuthController {
 
 
     }
+
+
+    @PutMapping("/{id}/set-profile-image")
+    public ResponseEntity setUserProfileImage(
+            @PathVariable String id, 
+            @RequestParam("image") MultipartFile image
+        ) {
+        
+        // get the user
+        Optional<User> user =  userRepository.findById(id);
+        // check if the user exists
+        if(!user.isPresent()){
+              return ResponseEntity
+                    .badRequest()
+                    .body(
+                        new ErrorResponse(
+                                404,
+                                "user/not-found",
+                                "User not found, invalid user id"
+                        )
+
+                    );
+
+        }
+        // get the user object
+        User userValues = user.get();
+
+        String message = "";
+        try {
+            File uploadFile = new File();
+            uploadFile.setName(image.getOriginalFilename());
+            uploadFile.setCreatedAt(new Date());
+            uploadFile.setContent(new Binary(image.getBytes()));
+            uploadFile.setContentType(image.getContentType());
+            uploadFile.setSize(image.getSize());
+
+            File savedFile = fileRepository.save(uploadFile);
+            userValues.setImage(savedFile.getId());
+            userRepository.save(userValues);
+
+            String url = "http://localhost:8089/api/v1/file/image/"+ savedFile.getId();
+            System.out.println(url);
+            userValues.setImage(url);
+
+            return ResponseEntity.ok(
+                userValues
+            );
+        } catch (Exception e) {
+        message = "Could not upload the file: " + image.getOriginalFilename() + "!";
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(
+            new ErrorResponse(
+                    404,
+                    "code",
+                    message
+            )
+            );
+        }
+    }
+
+
 }
