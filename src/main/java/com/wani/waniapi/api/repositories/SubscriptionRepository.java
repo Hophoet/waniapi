6183b3a6bd8cf530a8833b07b2cdd8bf79393f51
hophoet <hophoet@gmail.com>
2021-06-07 15:37:14 +0000

    (repository) - add findBySubscriptionPlanId method

--- src/main/java/com/wani/waniapi/api/repositories/SubscriptionRepository.java
+++ src/main/java/com/wani/waniapi/api/repositories/SubscriptionRepository.java
@@ -2,13 +2,16 @@ package com.wani.waniapi.api.repositories;
 
 
 import com.wani.waniapi.api.models.Subscription;
+import com.wani.waniapi.api.models.SubscriptionPlan;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
 import java.util.List;
 
 public interface SubscriptionRepository extends MongoRepository<Subscription, String> {
     List<Subscription> findByUserId(String userId);
+    List<Subscription> findBySubscriptionPlanId(String subscriptionPlanId);
+
 
 
 }
