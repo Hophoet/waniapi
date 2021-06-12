2e2aca5aeac2c438bf14bd2659eb5e2b6088ec6b
hophoet <hophoet@gmail.com>
2021-06-12 11:30:51 +0000

    (request) - add CreatePaymentMethodRequest for payment method request parameter type

---
+++ src/main/java/com/wani/waniapi/api/playload/request/paymentmethod/CreatePaymentMethodRequest.java
@@ -0,0 +1,41 @@
+package com.wani.waniapi.api.playload.request.paymentmethod;
+
+import javax.validation.constraints.NotBlank;
+
+public class CreatePaymentMethodRequest {
+
+	@NotBlank
+	private String name;
+	
+	@NotBlank
+	private String description;
+	
+	private Boolean isActive;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	
+	
+}
