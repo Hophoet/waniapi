33a9459e00adf914e686a880b2cf14cd8c38b714
hophoet <hophoet@gmail.com>
2021-06-20 17:04:42 +0000

    (controller, withdrawal) - add getUserWithdrawal endpoint method to handle user withdrawal getting request

--- src/main/java/com/wani/waniapi/api/controllers/WithdrawalController.java
+++ src/main/java/com/wani/waniapi/api/controllers/WithdrawalController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wani.waniapi.api.models.Withdrawal;
 import com.wani.waniapi.api.repositories.WithdrawalRepository;
+import com.wani.waniapi.auth.security.services.UserDetailsImpl;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,8 +27,21 @@ public class WithdrawalController {
      */
     @GetMapping("admin/withdrawals")
     @PreAuthorize("hasRole('ADMIN')")
-    public List<Withdrawal> getWithdrawals(){
+    public List<Withdrawal> getWithdrawals(
+    ){
         List<Withdrawal> withdrawals = withdrawalRepository.findAll();
+        return withdrawals;
+    }
+    
+	/*
+     * get user withdrawals 
+     */
+    @GetMapping("admin/user/withdrawals")
+    @PreAuthorize("hasRole('USER')")
+    public List<Withdrawal> getUserWithdrawals(
+ 	       @AuthenticationPrincipal UserDetailsImpl userDetail
+    ){
+        List<Withdrawal> withdrawals = withdrawalRepository.findByUserId(userDetail.getId());
         return withdrawals;
     }
 
