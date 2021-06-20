b82645c853ed42cb122da13ba47f9e838fd4bc69
hophoet <hophoet@gmail.com>
2021-06-20 17:48:56 +0000

    (models) - add phoneNumber and change paymentId by paymentMethodId, add getters and setter, set the contructor

--- src/main/java/com/wani/waniapi/api/models/Withdrawal.java
+++ src/main/java/com/wani/waniapi/api/models/Withdrawal.java
@@ -8,9 +8,24 @@ public class Withdrawal {
 	@Id
     private String id;
 	private String userId;
-    private String paymentId;
+    private String paymentMethodId;
+    private String phoneNumber;
 	private int amount;
     private long createdAt;
+    
+    public Withdrawal(
+    		String userId,
+    		String phoneNumber,
+    		String paymentMethodId,
+    		int amount
+    		) {
+    	this.userId = userId;
+    	this.phoneNumber = phoneNumber;
+    	this.setPaymentMethodId(paymentMethodId);
+    	this.amount = amount;
+    	this.createdAt = System.currentTimeMillis();
+    }
+    
 	public String getId() {
 		return id;
 	}
@@ -23,12 +38,7 @@ public class Withdrawal {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-	public String getPaymentId() {
-		return paymentId;
-	}
-	public void setPaymentId(String paymentId) {
-		this.paymentId = paymentId;
-	}
+
 	public int getAmount() {
 		return amount;
 	}
@@ -40,6 +50,20 @@ public class Withdrawal {
 	}
 	public void setCreatedAt(long createdAt) {
 		this.createdAt = createdAt;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPaymentMethodId() {
+		return paymentMethodId;
+	}
+
+	public void setPaymentMethodId(String paymentMethodId) {
+		this.paymentMethodId = paymentMethodId;
 	}
 
 }
