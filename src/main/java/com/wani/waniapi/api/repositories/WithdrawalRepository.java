16a0d9f97f3c40563aa46573e37c09e335ce0f3b
hophoet <hophoet@gmail.com>
2021-06-20 17:05:42 +0000

    (repository) - add findByUserId for handle user withdrawals getting request

--- src/main/java/com/wani/waniapi/api/repositories/WithdrawalRepository.java
+++ src/main/java/com/wani/waniapi/api/repositories/WithdrawalRepository.java
@@ -1,10 +1,11 @@
 package com.wani.waniapi.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.wani.waniapi.api.models.Withdrawal;
 
 public interface  WithdrawalRepository  extends MongoRepository<Withdrawal, String> {
-	  
-
+    List<Withdrawal> findByUserId(String userId);
 }
