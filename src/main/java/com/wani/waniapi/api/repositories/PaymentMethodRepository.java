61016258de4578bcd8d59d265abaac3fea983cec
hophoet <hophoet@gmail.com>
2021-07-13 17:20:25 +0000

    (repository) update PaymentMethodRepository add findByName to handle the payment method getting with it name attribute

--- src/main/java/com/wani/waniapi/api/repositories/PaymentMethodRepository.java
+++ src/main/java/com/wani/waniapi/api/repositories/PaymentMethodRepository.java
@@ -1,9 +1,13 @@
 package com.wani.waniapi.api.repositories;
 
 import com.wani.waniapi.api.models.PaymentMethod;
+
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
 public interface PaymentMethodRepository extends MongoRepository<PaymentMethod, String> {
+    Optional<PaymentMethod> findByName(String name);
 
 }
