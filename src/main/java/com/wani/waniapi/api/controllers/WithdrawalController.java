3c01254acb0a2a6c41fe3a387843d885aff1df03
hophoet <hophoet@gmail.com>
2021-07-16 14:26:19 +0000

    (controller, withdrawal) update createWithdrawal hande perfect money information setting validation during the request

--- src/main/java/com/wani/waniapi/api/controllers/WithdrawalController.java
+++ src/main/java/com/wani/waniapi/api/controllers/WithdrawalController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.wani.waniapi.api.models.Account;
 import com.wani.waniapi.api.models.Payment;
 import com.wani.waniapi.api.models.PaymentMethod;
+import com.wani.waniapi.api.models.PerfectMoneyAccount;
 import com.wani.waniapi.api.models.SubscriptionPlan;
 import com.wani.waniapi.api.models.Withdrawal;
 import com.wani.waniapi.api.playload.request.subscription.CreateSubscriptionRequest;
@@ -25,6 +26,7 @@ import com.wani.waniapi.api.playload.request.withdrawal.CreateWithdrawal;
 import com.wani.waniapi.api.repositories.AccountRepository;
 import com.wani.waniapi.api.repositories.PaymentMethodRepository;
 import com.wani.waniapi.api.repositories.PaymentRepository;
+import com.wani.waniapi.api.repositories.PerfectMoneyAccountRepository;
 import com.wani.waniapi.api.repositories.WithdrawalRepository;
 import com.wani.waniapi.auth.playload.response.ErrorResponse;
 import com.wani.waniapi.auth.security.services.UserDetailsImpl;
@@ -42,6 +44,8 @@ public class WithdrawalController {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    PerfectMoneyAccountRepository perfectMonayAccountRepository;
 	
 	/*
      * get all withdrawals 
@@ -100,6 +104,45 @@ public class WithdrawalController {
     	Optional<PaymentMethod> paymentMethod =  paymentMethodRepository.findById(
     			createWithdrawal.getPaymentMethodId()
     	);
+    	Optional<Account> account =  accountRepository.findById(createWithdrawal.getAccountId());
+    	if(!account.isPresent()) {
+    		return ResponseEntity
+    	              .badRequest()
+    	              .body(
+    	                  new ErrorResponse(
+    	                      404,
+    	                      "withdrawal/account-not-found",
+    	                      "invalid user account not found"
+    	                  )
+    	              );
+    	}
+    	Account accountValues = account.get();
+
+    	if(!accountValues.getUserId().contentEquals(userDetail.getId())) {
+    		return ResponseEntity
+  	              .badRequest()
+  	              .body(
+  	                  new ErrorResponse(
+  	                      401,
+  	                      "withdrawal/account-access-not-authorized",
+  	                      "withdrawal account access is not authorized"
+  	                  )
+  	              );
+    	}
+    	
+    	
+    	if(createWithdrawal.getAmount() > accountValues.getAmount()) {
+    		return ResponseEntity
+  	              .badRequest()
+  	              .body(
+  	                  new ErrorResponse(
+  	                      400,
+  	                      "withdrawal/not-enough-amount",
+  	                      "not enough amount available for the withdrawal"
+  	                  )
+  	              );
+    	}
+    	// check payment method validataion
     	if(!paymentMethod.isPresent()){
     		return ResponseEntity
               .badRequest()
@@ -111,35 +154,43 @@ public class WithdrawalController {
                   )
               );
     	}
-    	Optional<Account> account =  accountRepository.findById(createWithdrawal.getAccountId());
-    	if(!account.isPresent()) {
-    		return ResponseEntity
-    	              .badRequest()
-    	              .body(
-    	                  new ErrorResponse(
-    	                      401,
-    	                      "withdrawal/account-not-found",
-    	                      "invalid user account not found"
-    	                  )
-    	              );
-    	}
-    	Account accountValues = account.get();
-    	if(createWithdrawal.getAmount() > accountValues.getAmount()) {
-    		return ResponseEntity
-  	              .badRequest()
-  	              .body(
-  	                  new ErrorResponse(
-  	                      401,
-  	                      "withdrawal/not-enough-amount",
-  	                      "not enough amount available for the withdrawal"
-  	                  )
-  	              );
-    	}
+    	
+        // get the payment method object
+        PaymentMethod paymentMethodObject = paymentMethod.get(); 
+        if(!paymentMethodObject.getCode().contentEquals("PM")){
+			return ResponseEntity
+				.badRequest()
+				.body(
+					new ErrorResponse(
+							400,
+							"payment-method/not-available",
+							"payment method is not avialable"
+					)
+				);
+        }
+        
+        // check if the user have a perfect money account
+		// create new account object
+		Optional<PerfectMoneyAccount> perfectMoneyAccount = perfectMonayAccountRepository.findByUserId(userDetail.getId());
+		if(!perfectMoneyAccount.isPresent()) {
+			return ResponseEntity
+				.badRequest()
+				.body(
+					new ErrorResponse(
+							400,
+							"perfect-money-account/not-set",
+							"perfect-money account must be found"
+					)
+				);
+		}
+
+        //TODO Make the perfect money api withdrawal payment to the user account
+    	
     	
     	// create the withdrawal
     	Withdrawal withdrawal = new Withdrawal(
     			accountValues.getId(), 
-    			createWithdrawal.getPaymentMethodId(), 
+    			paymentMethodObject.getId(), 
     			createWithdrawal.getAmount());
     	
     	Withdrawal createdWithdrawal = withdrawalRepository.save(withdrawal);
