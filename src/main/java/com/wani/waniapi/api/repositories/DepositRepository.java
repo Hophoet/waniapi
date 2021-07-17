46f5dfa05c8110ff0b2fcea95fa032ba8f0a3cf5
hophoet <hophoet@gmail.com>
2021-07-17 16:21:14 +0000

    (repository) remove unused repository DepositRepository

--- src/main/java/com/wani/waniapi/api/repositories/DepositRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wani.waniapi.api.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.wani.waniapi.api.models.Deposit;
-
-public interface DepositRepository extends MongoRepository<Deposit, String> {
-
-}
